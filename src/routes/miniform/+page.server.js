dc1426c99a05ba189fe8079934b2bbe91c8b2c0a
Thomas Pettersson <thomas.pettesson@actualize.de>
2023-05-09 15:18:50 +0200

    nodemailer await

--- src/routes/miniform/+page.server.js
+++ src/routes/miniform/+page.server.js
@@ -1,3 +1,4 @@
+import { PRIVATE_EMAIL_PW } from '$env/static/private';
 import nodemailer from 'nodemailer';
 const transporter = nodemailer.createTransport({
   service: 'Actualize',
@@ -7,35 +8,34 @@ const transporter = nodemailer.createTransport({
   secure: true,
   auth: {
     user: 'thomas.pettersson@actualize.de',
-    pass: 'emailPETAsson1802'
+    pass: PRIVATE_EMAIL_PW
   }
 })
-
-
 
 export const actions = {
   default: async ({ request }) => {
     const values = await request.formData();
-
     const name = values.get("full-name") ? values.get("full-name") : '';
     const number = values.get("phone") ? values.get("phone") : '';
     const email = values.get("email") ? values.get("email") : '';
     const text = values.get("message") ? values.get("message") : '';
-
-
     const message = "Name: " + name + "\n\nTelefon: " + number + "\n\nE-Mail-Adresse: " + email + "\n\nNachricht:\n\n" + text;
 
-    transporter.sendMail({
-      from: email,
-      to: 'tom@actualize.de',
-      subject: 'Nachricht aus dem Kontaktformular',
-      text: message,
-    }, (error, info) => {
-      if (error) {
-        console.log(error);
-      } else {
-        console.log('Email sent: ' + info.response);
-      }
-    })
+    await new Promise((resolve, reject) => {
+      transporter.sendMail({
+        from: email,
+        to: 'tom@actualize.de',
+        subject: 'Nachricht aus dem Kontaktformular',
+        text: message,
+      }, (error, info) => {
+        if (error) {
+          console.log(error);
+          reject(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+          resolve(info);
+        }
+      })
+    });
   }
 };
