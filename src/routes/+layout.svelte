f38caede83f27521248ac70375d2e79bc5e6bb4e
Thomas Pettersson <thomas.pettersson@actualize.de>
2023-10-15 17:27:22 +0200

    add betriebsferien banne

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,9 @@
 	import '../app.css';
 	import Footer from '../lib/Footer.svelte';
 	import Navbar from '../lib/Navbar.svelte';
+    import Message from '../lib/Message.svelte';
 </script>
-
+<Message />
 <Navbar />
 <slot />
 <Footer />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
 	import Cards from './../lib/Cards.svelte';
 	import About from '../lib/About.svelte';
-	import Hero from '../lib/Hero.svelte';
+    import Hero from '../lib/Hero.svelte';
 	import ContactForm from '../lib/ContactForm.svelte';
 </script>
 
