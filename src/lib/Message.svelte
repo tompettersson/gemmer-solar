753b278c5dba934c829fc3bab76859b53a748f61
Thomas Pettersson <thomas.pettersson@actualize.de>
2023-10-15 17:29:40 +0200

    not uppercase

--- src/lib/Message.svelte
+++ src/lib/Message.svelte
@@ -1,7 +1,7 @@
 <!---- HERO ----->
 <section>
 	<div
-		class="flex border border-transparent justify-center bg-gemmer-400 px-10 py-1 text-base font-normal uppercase text-zinc-900  transition-colors hover:bg-gemmer-500 md:py-3"
+		class="flex border border-transparent justify-center bg-gemmer-400 px-10 py-1 text-base font-normal text-zinc-900  transition-colors hover:bg-gemmer-500 md:py-3"
 	>
 		Wir machen vom 18.10.23 bis 29.10.23 Betriebsferien. Ab dem 30.10.23 sind wir wieder für Sie da.
 	</div>
